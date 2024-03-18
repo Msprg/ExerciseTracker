776ad52131f2a7c9018b7bf76ceefa859b7f30fd
Msprg <18015488+Msprg@users.noreply.github.com>
2024-03-18 05:34:23 +0100

    [ADD] History material3 datePicker

--- app/src/main/java/com/msprg/exercisetracker/ui/screens/History.kt
+++ app/src/main/java/com/msprg/exercisetracker/ui/screens/History.kt
@@ -1,18 +1,58 @@
 package com.msprg.exerciseTracker.ui.screens
 
-import androidx.compose.foundation.layout.Box
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.msprg.exerciseTracker.ui.theme.ExerciseTrackerTheme
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HistoryScreen() {
-    Box(modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Hist_CONTENT", fontSize = 32.sp)
+    Scaffold(modifier = Modifier.fillMaxSize(),
+        content = { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .padding(paddingValues)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                val datePickerState = rememberDatePickerState()
+                DatePicker(
+                    state = datePickerState,
+                    modifier = Modifier.padding(16.dp),
+                    title = null
+                )
+
+                Text(
+                    "Selected date timestamp: ${datePickerState.selectedDateMillis ?: "no selection"}",
+                    modifier = Modifier.align(Alignment.CenterHorizontally),
+                    fontSize = 19.sp
+                )
+            }
+        }
+    )
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HistoryScreenPrew() {
+    ExerciseTrackerTheme {
+        HistoryScreen()
     }
 }
+
